bb21817251f01aa6e89f3d772a7b44c6e88f382c
indradev_prasad <kumarindradevd9211@gmail.com>
2017-06-01 10:29:52 +0530

    first

---
+++ index.php
@@ -0,0 +1,11 @@
+   
+   <?php 
+//return size of subfolders
+   function folderSize($dir) {
+        $size = 0;
+        foreach (glob(rtrim($dir, '/') . '/*', GLOB_NOSORT) as $each) {
+            $size += is_file($each) ? filesize($each) : folderSize($each);
+        }
+        return $size; //return in bytes
+    }
+    ?
